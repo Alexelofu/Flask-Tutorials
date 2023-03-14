a4920392b607d876b36b9e157e43f4cec1403887
Alexelofu <ofuonyeadialexander1997@gmail.com>
2023-03-14 19:38:44 +0100

    New Update

--- CS50_Tutorial/app.py
+++ CS50_Tutorial/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from cs50 import SQL
 
 app = Flask(__name__)
+
+#create db variable that opens sports db
+
+db = SQL("sqlite:///sports.db")
 
 REGISTRANTS = {}
 
@@ -16,16 +21,19 @@ def index():
 
 @app.route("/register", methods = ["POST"])
 def register():
-    name = request.form.get("name")
-    if not name:
+    name = request.form.get("name").title()
+    sport = request.form.get("sport").title()
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
-    sport = request.form.get("sport")
+    
+    # Allow us insert values into db
 
-    if sport not in SPORTS:
-        return render_template("failure.html")
-    REGISTRANTS[name] = sport
-    return render_template("success.html")
+    db.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport)
+
+    # Confirm registration(redirection)
+    return redirect("/registrants")
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    registrants = db.execute("SELECT * FROM registrants")
+    return render_template("registrants.html", registrants=registrants)
